f6d663fa29e7a44918bf828b344abcdedd1d5f2a
Youssef Bouira <95041290+BoodieBoodieBoo@users.noreply.github.com>
2022-01-26 17:58:38 +0100

    Create khabarovsk.js

---
+++ Libreria JS/khabarovsk.js
@@ -0,0 +1,7 @@
+        function Sakhalin() {
+            let dado=0;
+            let casuale=0;
+            casuale=Math.random();
+            dado=Math.round(casuale*1);
+            document.getElementById("yakutzk").innerHTML=dado+ "<br>";
+        }
